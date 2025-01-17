dcc2af6ee1398bc1d10797bffe693f83ed8f8659
lukas-al <60654006+lukas-al@users.noreply.github.com>
2025-01-17 00:06:03 +0000

    Errors in the final loadMNIST method of data, tidied up rest of implementation

--- data.zig
+++ data.zig
@@ -1,6 +1,7 @@
 //! Data loader for MNIST
 const std = @import("std");
 
+/// Decode the IDX types stored as a hex value
 pub fn TypeFromHex(hex: u8) ?type {
     return switch (hex) {
         0x08 => u8,
@@ -13,107 +14,460 @@ pub fn TypeFromHex(hex: u8) ?type {
     };
 }
 
+/// Image struct
+pub const Image = struct {
+    width: u8 = 28,
+    height: u8 = 28,
+    pixels: []u8,
+};
+
+/// Create a single dataset struct
+pub const Dataset = struct {
+    images: []Image,
+    labels: []u8,
+
+    /// Free allocated memory
+    pub fn deinit(self: *Dataset, allocator: std.mem.Allocator) void {
+        for (self.images) |image| {
+            allocator.free(image.pixels);
+        }
+        allocator.free(self.images);
+        allocator.free(self.labels);
+    }
+};
+
+/// MNIST Loaders and such
 pub const MNISTData = struct {
-    training_images: [][]f32,
-    training_labels: []f32,
-    test_images: [][]f32,
-    test_labels: []f32,
-
     // !IDX FORMAT INFO: https://www.fon.hum.uva.nl/praat/manual/IDX_file_format.html
-    // pub fn deinit(self: *MNISTData) void {
-    //     // Free the training images arrays
-    //     for (self.training_images) |image| {
-    //         self.allocator.free(image);
-    //     }
-    //     self.allocator.free(self.training_images);
-    //     self.allocator.free(self.training_labels);
+    /// Struct for MetadataReturn types.
+    const MetadataReturn = union(enum) { 
+        image_file: struct {
+            type_info: u8,
+            num_images: u32,
+            width: u8,
+            height: u8,
+        },
+        label_file: struct {
+            type_info: u8,
+            num_labels: u32,
+        }
+    };
 
-    //     // Free the test images arrays
-    //     for (self.test_images) |image| {
-    //         self.allocator.free(image);
-    //     }
-    //     self.allocator.free(self.test_images);
-    //     self.allocator.free(self.test_labels);
-    // }
+    /// Some custom error types
+    const MNISTError = error{
+        InvalidDataType,
+        InvalidDimensions,
+        InvalidFileType,
+        IncompleteRead,
+    };
 
-    // /// Extract out same logic
-    // fn readHeader(reader: anytype) !struct { u32, u32 } {
-    //     // Read magic number and number of items (big endian)
-    //     const magic = try reader.readIntBig(u32);
-    //     const num_items = try reader.readIntBig(u32);
-    //     return .{ magic, num_items };
-    // }
+    /// Read the header of our file
+    fn readMetadata(reader: anytype) !MetadataReturn {
+        const magic = try reader.readIntBig(u32); // Read magic number which contains type info and dimensionality
+        const magic_bytes = @as([4]u8, @bitCast(magic)); // Convert to array of bytes for easy access
 
-    // /// Extract out logic
-    // fn readImageDimensions(reader: anytype) !struct { u32, u32 } {
-    //     // Read number of rows and columns (big endian)
-    //     const num_rows = try reader.readIntBig(u32);
-    //     const num_cols = try reader.readIntBig(u32);
-    //     return .{ num_rows, num_cols };
-    // }
+        const data_type = magic_bytes[2];
+        const num_dims = magic_bytes[3];
 
-    /// Extract out logic
-    fn readMetadata(reader: anytype) !struct { u32, u32, u32, u32 } {
-        // Read out all metadata from the file at once
-        const magic = try reader.readIntBig(u32);
-        const num_items = try reader.readIntBig(u32);
-        const num_rows = try reader.readIntBig(u32);
-        const num_cols = try reader.readIntBig(u32);
+        // Validate data type // TODO: What type are we expecting? Can we assert that?
+        if (TypeFromHex(data_type) == null) {
+            return MNISTError.InvalidDataType;
+        }
 
-        return .{ magic, num_items, num_rows, num_cols };
+        switch (num_dims) {
+            // label
+            1 => {
+                const num_labels = try reader.readIntBig(u32);
+                return MetadataReturn{ .label_file = .{ .type_info = data_type, .num_labels = num_labels } };
+            },
+            3 => {
+                const num_images = try reader.readIntBig(u32);
+                const height = try reader.readIntBig(u32);
+                const width = try reader.readIntBig(u32);
+                return MetadataReturn{ .image_file = .{
+                    .type_info = data_type,
+                    .num_images = num_images,
+                    .height = height,
+                    .width = width,
+                } };
+            },
+            else => return MNISTError.InvalidDimensions,
+        }
     }
 
-    /// Logic to decode each individual image and shape it into the NN input data
-    pub fn decodeImageBinary(filepath: []u8, allocator: std.mem.Allocator) [][]f32 {
+    /// Load image data from IDX file
+    pub fn loadImages(filepath: []const u8, allocator: std.mem.Allocator) ![]Image {
         const file = try std.fs.cwd().openFile(filepath);
         defer file.close();
         var file_reader = file.reader();
 
-        const file_metadata = try readMetadata(file_reader);
+        const metadata = try readMetadata(&file_reader);
+        // if (metadata.tag != .image_file) return error.InvalidFileType;
 
-        // Check that the metadata is correct
-        // First two bytes are set to 0.
-        if (file_metadata[0] != 0) return error.InvalidInput;
-        if (file_metadata[1] != 0) return error.InvalidInput;
+        const num_images = metadata.image_file.num_images;
+        const height = metadata.image_file.height;
+        const width = metadata.image_file.width;
 
-        // Third byte defines data type.
-        const data_type = TypeFromHex(file_metadata[2]);
+        var images = try allocator.alloc(Image, num_images);
+        errdefer allocator.free(images);
 
-        // Fourth defines number of dimensions of the stored arrays
-        const array_dim = file_metadata[3];
-        if (array_dim != )
+        for (0..num_images) |i| {
+            var pixels = try allocator.alloc(u8, width * height);
+            _ = &pixels; // Overwrite the error
+            errdefer allocator.free(pixels);
 
-        // Assign a memory space array of the expected file length
+            const num_bytes_read = try file_reader.readAll(pixels);
+            if (num_bytes_read != width * height) return MNISTError.IncompleteRead;
 
+            images[i] = Image{
+                .width = @intCast(width),
+                .height = @intCast(height),
+                .pixels = pixels,
+            };
+        }
+
+        return images;
     }
 
-    /// Logic to decode
-    pub fn decodeLabelBinary(filepath: []u8) []f32 {
-        //
+    /// Load label data from IDX file
+    pub fn loadLabels(filepath: []const u8, allocator: std.mem.Allocator) ![]u8 {
+        const file = try std.fs.cwd().openFile(filepath);
+        defer file.close();
+        var file_reader = file.reader();
+
+        const metadata = try readMetadata(&file_reader);
+        // if (metadata.tag != .label_file) return MNISTError.InvalidFileType;
+
+        const num_labels = metadata.label_file.num_labels;
+
+        var labels = try allocator.alloc(u8, num_labels);
+        _ = &labels;
+        errdefer allocator.free(labels);
+
+        const num_bytes_read = try file_reader.readAll(labels);
+        if (num_bytes_read != num_labels) return MNISTError.IncompleteRead;
+
+        return labels;
     }
 
-    // pub fn load(train_images_path: []u8, train_labels_path: []u8, test_images_path: []u8, test_labels_path: []u8, allocator: std.mem.Allocator) !MNISTData {
-    //     const train_images_file = try std.fs.cwd().openFile(train_images_path);
-    //     defer train_images_file.close();
-    //     var train_images_reader = train_images_file.reader();
+    pub fn loadMNIST(
+        train_images_path: []const u8, 
+        train_labels_path: []const u8, 
+        test_images_path: []const u8, 
+        test_labels_path: []const u8, 
+        allocator: std.mem.Allocator,
+    ) !struct { train: Dataset, test: Dataset } {
 
-    //     const train_labels_file = try std.fs.cwd().openFile(train_labels_path);
-    //     defer train_labels_file.close();
-    //     var train_labels_reader = train_labels_file.reader();
+        // Instantiate an empty dataset structure to populate
+        var train_dataset = Dataset{
+            .images = try loadImages(train_images_path, allocator),
+            .labels = try loadLabels(train_labels_path, allocator),
+        };
+        errdefer train_dataset.deinit(allocator);
 
-    //     const test_images_file = try std.fs.cwd().openFile(test_images_path);
-    //     defer test_images_file.close();
-    //     var test_images_reader = test_images_file.reader();
+        // Load test data
+        var test_dataset = Dataset{
+            .images = try loadImages(test_images_path, allocator),
+            .labels = try loadLabels(test_labels_path, allocator), 
+        };
+        errdefer test_dataset.deinit(allocator);
 
-    //     const test_labels_file = try std.fs.cwd().openFile(test_labels_path);
-    //     defer test_labels_file.close();
-    //     var test_labels_reader = test_labels_file.reader();
-
-    //     // Now to decode the datasets from their binary format. We don't really care about most of the information in the headers.
-    //     // Read headers
-    //     const train_images_metadata = readMetadata(train_images_reader);
-    //     const train_labels_metadata = readMetadata(train_labels_reader);
-    //     const test_images_metadata = readMetadata(test_images_reader);
-    //     const test_labels_metadata = readMetadata(test_labels_reader);
-    // }
+        return .{
+            .train = train_dataset,
+            .test = test_dataset,
+        };
+    }
 };
+
+//! Data loader for MNIST
+const std = @import("std");
+
+/// Decode the IDX types stored as a hex value
+pub fn TypeFromHex(hex: u8) ?type {
+    return switch (hex) {
+        0x08 => u8,
+        0x09 => i8,
+        0x0B => i16,
+        0x0C => i32,
+        0x0D => f32,
+        0x0E => f64,
+        else => null,
+    };
+}
+
+/// Image struct
+pub const Image = struct {
+    width: u8 = 28,
+    height: u8 = 28,
+    pixels: []u8,
+};
+
+/// Create a single dataset struct
+pub const Dataset = struct {
+    images: []Image,
+    labels: []u8,
+
+    /// Free allocated memory
+    pub fn deinit(self: *Dataset, allocator: std.mem.Allocator) void {
+        for (self.images) |image| {
+            allocator.free(image.pixels);
+        }
+        allocator.free(self.images);
+        allocator.free(self.labels);
+    }
+};
+
+/// MNIST Loaders and such
+pub const MNISTData = struct {
+    // !IDX FORMAT INFO: https://www.fon.hum.uva.nl/praat/manual/IDX_file_format.html
+    /// Struct for MetadataReturn types.
+    const MetadataReturn = union(enum) { 
+        image_file: struct {
+            type_info: u8,
+            num_images: u32,
+            width: u8,
+            height: u8,
+        },
+        label_file: struct {
+            type_info: u8,
+            num_labels: u32,
+        }
+    };
+
+    /// Some custom error types
+    const MNISTError = error{
+        InvalidDataType,
+        InvalidDimensions,
+        InvalidFileType,
+        IncompleteRead,
+    };
+
+    /// Read the header of our file
+    fn readMetadata(reader: anytype) !MetadataReturn {
+        const magic = try reader.readIntBig(u32); // Read magic number which contains type info and dimensionality
+        const magic_bytes = @as([4]u8, @bitCast(magic)); // Convert to array of bytes for easy access
+
+        const data_type = magic_bytes[2];
+        const num_dims = magic_bytes[3];
+
+        // Validate data type
+        if (TypeFromHex(data_type) == null) {
+            return MNISTError.InvalidDataType;
+        }
+
+        switch (num_dims) {
+            // label
+            1 => {
+                const num_labels = try reader.readIntBig(u32);
+                return MetadataReturn{ .label_file = .{ .type_info = data_type, .num_labels = num_labels } };
+            },
+            3 => {
+                const num_images = try reader.readIntBig(u32);
+                const height = try reader.readIntBig(u32);
+                const width = try reader.readIntBig(u32);
+                return MetadataReturn{ .image_file = .{
+                    .type_info = data_type,
+                    .num_images = num_images,
+                    .height = @intCast(height),
+                    .width = @intCast(width),
+                } };
+            },
+            else => return MNISTError.InvalidDimensions,
+        }
+    }
+
+    /// Load image data from IDX file
+    pub fn loadImages(filepath: []const u8, allocator: std.mem.Allocator) ![]Image {
+        const file = try std.fs.cwd().openFile(filepath, .{});
+        defer file.close();
+        var file_reader = file.reader();
+
+        const metadata = try readMetadata(&file_reader);
+        switch (metadata) {
+            .image_file => |info| {
+                const num_images = info.num_images;
+                const height = info.height;
+                const width = info.width;
+
+                var images = try allocator.alloc(Image, num_images);
+                errdefer allocator.free(images);
+
+                for (0..num_images) |i| {
+                    var pixels = try allocator.alloc(u8, width * height);
+                    errdefer allocator.free(pixels);
+
+                    const num_bytes_read = try file_reader.readAll(pixels);
+                    if (num_bytes_read != width * height) return MNISTError.IncompleteRead;
+
+                    images[i] = Image{
+                        .width = width,
+                        .height = height,
+                        .pixels = pixels,
+                    };
+                }
+
+                return images;
+            },
+            .label_file => return MNISTError.InvalidFileType,
+        }
+    }
+
+    /// Load label data from IDX file
+    pub fn loadLabels(filepath: []const u8, allocator: std.mem.Allocator) ![]u8 {
+        const file = try std.fs.cwd().openFile(filepath, .{});
+        defer file.close();
+        var file_reader = file.reader();
+
+        const metadata = try readMetadata(&file_reader);
+        switch (metadata) {
+            .label_file => |info| {
+                const num_labels = info.num_labels;
+                var labels = try allocator.alloc(u8, num_labels);
+                errdefer allocator.free(labels);
+
+                const num_bytes_read = try file_reader.readAll(labels);
+                if (num_bytes_read != num_labels) return MNISTError.IncompleteRead;
+
+                return labels;
+            },
+            .image_file => return MNISTError.InvalidFileType,
+        }
+    }
+
+    /// Load both training and test datasets
+    pub fn loadMNIST(
+        train_images_path: []const u8, 
+        train_labels_path: []const u8, 
+        test_images_path: []const u8, 
+        test_labels_path: []const u8, 
+        allocator: std.mem.Allocator,
+    ) !struct { train: Dataset, test: Dataset } {
+        // Load training data
+        var train_dataset = Dataset{
+            .images = try loadImages(train_images_path, allocator),
+            .labels = try loadLabels(train_labels_path, allocator),
+        };
+        errdefer train_dataset.deinit(allocator);
+
+        // Load test data
+        var test_dataset = Dataset{
+            .images = try loadImages(test_images_path, allocator),
+            .labels = try loadLabels(test_labels_path, allocator),
+        };
+        errdefer test_dataset.deinit(allocator);
+
+        return .{
+            .train = train_dataset,
+            .test = test_dataset,
+        };
+    }
+};
+
+// Tests
+test "Load and validate MNIST data" {
+    // Setup paths to your MNIST data files
+    const train_images_path = "data/train-images-idx3-ubyte";
+    const train_labels_path = "data/train-labels-idx1-ubyte";
+    const test_images_path = "data/t10k-images-idx3-ubyte";
+    const test_labels_path = "data/t10k-labels-idx1-ubyte";
+
+    // Load the data
+    const data = try MNISTData.loadMNIST(
+        train_images_path,
+        train_labels_path,
+        test_images_path,
+        test_labels_path,
+        std.testing.allocator,
+    );
+    defer data.train.deinit(std.testing.allocator);
+    defer data.test.deinit(std.testing.allocator);
+
+    // Verify training data
+    try validateDataset("Training", &data.train, 60000);
+    
+    // Verify test data
+    try validateDataset("Test", &data.test, 10000);
+
+    // Print some example digits
+    printExampleDigits(&data.train);
+}
+
+/// Helper function to validate dataset's structure and contents
+fn validateDataset(name: []const u8, dataset: *const Dataset, expected_size: usize) !void {
+    std.debug.print("\nValidating {s} Dataset:\n", .{name});
+    
+    // Check dataset size
+    try std.testing.expect(dataset.images.len == expected_size);
+    try std.testing.expect(dataset.labels.len == expected_size);
+    std.debug.print("✓ Dataset size matches expected ({d} samples)\n", .{expected_size});
+
+    // Validate image dimensions
+    for (dataset.images, 0..) |image, i| {
+        try std.testing.expect(image.width == 28);
+        try std.testing.expect(image.height == 28);
+        try std.testing.expect(image.pixels.len == 28 * 28);
+        
+        // Validate pixel values are in range 0-255
+        for (image.pixels) |pixel| {
+            try std.testing.expect(pixel <= 255);
+        }
+
+        // Validate label is 0-9
+        try std.testing.expect(dataset.labels[i] <= 9);
+    }
+    std.debug.print("✓ All images are 28x28 with valid pixel values\n", .{});
+    std.debug.print("✓ All labels are valid digits (0-9)\n", .{});
+
+    // Basic statistical checks
+    var label_counts = [_]usize{0} ** 10;
+    var total_pixel_sum: usize = 0;
+    var non_zero_pixels: usize = 0;
+
+    for (dataset.images, dataset.labels) |image, label| {
+        label_counts[label] += 1;
+        for (image.pixels) |pixel| {
+            total_pixel_sum += pixel;
+            if (pixel > 0) non_zero_pixels += 1;
+        }
+    }
+
+    // Verify we have samples of all digits
+    for (label_counts, 0..) |count, digit| {
+        try std.testing.expect(count > 0);
+        std.debug.print("  Digit {d}: {d} samples\n", .{digit, count});
+    }
+
+    const avg_pixel_value = @as(f32, @floatFromInt(total_pixel_sum)) / 
+        @as(f32, @floatFromInt(expected_size * 28 * 28));
+    const pixel_density = @as(f32, @floatFromInt(non_zero_pixels)) / 
+        @as(f32, @floatFromInt(expected_size * 28 * 28)) * 100;
+
+    std.debug.print("✓ Average pixel value: {d:.2}\n", .{avg_pixel_value});
+    std.debug.print("✓ Pixel density: {d:.1}%\n", .{pixel_density});
+}
+
+/// Helper function to print example digits as ASCII art
+fn printExampleDigits(dataset: *const Dataset) void {
+    std.debug.print("\nExample Digits:\n", .{});
+    const examples_to_print = 5;
+    
+    for (0..examples_to_print) |i| {
+        const image = dataset.images[i];
+        const label = dataset.labels[i];
+        
+        std.debug.print("\nDigit {d}:\n", .{label});
+        for (0..28) |y| {
+            for (0..28) |x| {
+                const pixel = image.pixels[y * 28 + x];
+                // Convert pixel value to ASCII character based on intensity
+                const char: u8 = if (pixel < 50) ' ' 
+                    else if (pixel < 100) '.' 
+                    else if (pixel < 150) '+' 
+                    else if (pixel < 200) '*' 
+                    else '#';
+                std.debug.print("{c}", .{char});
+            }
+            std.debug.print("\n", .{});
+        }
+    }
+}
